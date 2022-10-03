3676b85a68bea73918b83034866ee54451bad094
Parth <parthbhuva97@gmail.com>
2022-10-03 17:09:07 +0530

    Initial Commit

---
+++ app/src/main/java/com/shareit/file_sharing_app/Splash.kt
@@ -0,0 +1,22 @@
+package com.shareit.file_sharing_app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class Splash : AppCompatActivity() {
+    private lateinit var image : ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        image = findViewById(R.id.logo)
+        image.alpha = 0f
+        image.animate().setDuration(2000).alpha(1f).withEndAction{
+            startActivity(Intent(this@Splash,Home::class.java))
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
