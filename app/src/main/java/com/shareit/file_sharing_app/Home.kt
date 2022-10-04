cca218d9f4634e663bd928bad025695e6d326bdb
Parth <parthbhuva97@gmail.com>
2022-10-04 10:23:45 +0530

    Enabled a prompt for sender to input name of file to be sent

--- app/src/main/java/com/shareit/file_sharing_app/Home.kt
+++ app/src/main/java/com/shareit/file_sharing_app/Home.kt
@@ -1,11 +1,33 @@
 package com.shareit.file_sharing_app
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.textfield.TextInputEditText
 
 class Home : AppCompatActivity() {
+    private lateinit var sendButton : FloatingActionButton
+    private lateinit var receiveButton : FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        sendButton = findViewById(R.id.sendButton)
+        receiveButton = findViewById(R.id.receiveButton)
+
+        sendButton.setOnClickListener {
+            val mDialogView = LayoutInflater.from(this@Home).inflate(R.layout.send_files,null)
+            val mBuilder = AlertDialog.Builder(this@Home)
+                .setView(mDialogView)
+                .setTitle("Enter File Name :")
+            val mAlertDialog = mBuilder.show()
+            val verificationBtn = mDialogView.findViewById<Button>(R.id.selectFilesButton)
+            val et_code = mDialogView.findViewById<TextInputEditText>(R.id.nameInput)
+
+        }
     }
 }
